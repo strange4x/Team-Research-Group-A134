c140cafebfea1610b72a3ce9786a9971f6a9f2ac
Laban Mahihu <lm24aam@herts.ac.uk>
2024-11-16 14:27:32 +0000

    update: additional R code and README file changes.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Team Research Group A134
 
 **CSV file: Toyota.csv, DS138**
+For the complete dataset, [click here](https://www.kaggle.com/datasets/adityadesai13/used-car-dataset-ford-and-mercedes?select=toyota.csv)
 
 Members:
 
@@ -9,3 +10,10 @@ Members:
 - Emmanuel Chinedu -> 23080370
 - Martin Thuranira -> 23099101
 - Onatemowo Oluwadamilare Temidayo -> 23004860
+
+## Research Question
+
+Is there a significant difference in means in the average tax of used Toyota Cars in the UK with different mileage values?
+
+**Independent Variable**: Mileage.
+**Dependent Variable**: Tax.
--- comparison_of_means.R
+++ comparison_of_means.R
@@ -1,4 +1,26 @@
-# Importing the dataset
+# ------------------------------------------------------------------------------
+## Importing the dataset
 toyota <- read.csv('toyota.csv')
 #Selecting the dependent and independent variable
 df <- subset(toyota, select = c(tax, mileage))
+
+# ------------------------------------------------------------------------------
+## Visualization plots
+
+
+# ------------------------------------------------------------------------------
+## Splitting the data into training and test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+
+split <- sample.split(df$tax, SplitRatio = 0.8)
+training_set <- subset(df, split == TRUE)
+test_set <- subset(df, split == FALSE)
+
+## Regressor
+model <- lm(
+    formula = tax ~ mileage,
+    data = training_set
+)
+summary(model)
